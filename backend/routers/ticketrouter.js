8b47cf97cd7b2eb05e9809763a0f24fc4a032f3a
VenkateshR07 <venkatesh1422001@gmail.com>
2025-02-18 21:26:52 +0530

    Update ticketrouter.js

--- backend/routers/ticketrouter.js
+++ backend/routers/ticketrouter.js
@@ -6,7 +6,6 @@ const ticketRouter = express.Router()
 
 ticketRouter.post("/postticket",createtickentfun)
 ticketRouter.get("/getticket/:cellNumber",getticketfun)
-ticketRouter.put("/editticket/:id",editticketfun)
 
 
 module.exports = ticketRouter
